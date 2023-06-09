88bdc796969b8ac0230638583525bf21d69d74ff
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-06-09 09:43:27 +0530

    Create code for next and back press button.ino

---
+++ ZZ - On Progress Project Resources Folder/code for next and back press button.ino
@@ -0,0 +1,53 @@
+
+
+// const int ledPin = 13;
+
+// forword botton
+int fbPin = 2;
+int fbState = 0;
+int lastfbState = LOW;
+bool fbOn = false;
+
+// backward botton
+int bbPin = 7;
+int bbState = 0;
+int lastbbState = LOW;
+bool bbOn = false;
+
+void setup() {
+  // pinMode(ledPin, OUTPUT);
+  pinMode(fbPin, INPUT);
+  pinMode(bbPin, INPUT);
+}
+
+bool readingpress(int NewbuttonState, int lastButtonState, bool buttonOn);
+
+void loop() {
+  fbState = digitalRead(fbPin);
+  bbState = digitalRead(bbPin);
+
+  fbOn = readingpress(fbState, lastfbState, fbOn);
+  bbOn = readingpress(bbState, lastbbState, bbOn);
+  // fbOn == true ? : ;
+  // bbOn == true ? : ;
+  
+  lastfbState = fbState;
+  lastbbState = bbState;
+}
+
+bool readingpress(int NewbuttonState, int lastButtonState, bool buttonOn){
+  if (NewbuttonState != lastButtonState) {
+    if (NewbuttonState == HIGH) {
+      if (!buttonOn) {
+        // digitalWrite(ledPin, HIGH);
+        buttonOn = true;
+      } else {
+        // digitalWrite(ledPin, LOW);
+        buttonOn = false;
+      }
+    }
+    delay(50);
+  }
+
+  return buttonOn;
+}
