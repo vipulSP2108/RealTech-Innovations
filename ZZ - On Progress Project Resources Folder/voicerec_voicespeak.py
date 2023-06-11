85dfb4c63649fd965ee3663a9bc8a2048a086985
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-06-11 09:52:29 +0530

    Create voicerec_voicespeak.py

---
+++ ZZ - On Progress Project Resources Folder/voicerec_voicespeak.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import os
+
+if __name__ == "__main__":
+    # while True:
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print('Please say')
+        audio = r.listen(source)
+        vice = 'error'
+        try:
+            vice = r.recognize_google(audio)
+            print('you had said '+ vice)
+
+        except Exception as e:
+            print('error : ' + str(e))
+        
+    # command = 'say'
+    os.system(f'say {vice}')
